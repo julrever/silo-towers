84f6ad438da7ab6b777709488615abd4dc789bcd
jul <yu-@live.ru>
2018-09-20 22:17:25 +0300

    design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,32 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public towers: Indicator[];
+  public height: number[] = new Array(8);
+  title = 'silo-towers';
+
   constructor(private httpClient: HttpClient){}
-  getData(){
-    this.httpClient.get('http://demo1110987.mockable.io/silo-towers').subscribe(
-      (data:any[]) => {
-        console.log(data);
+
+   getData(){
+    this.httpClient.get('/db1.json').subscribe(
+      (data) => {
+        this.towers = data as Array<Indicator>;
+        for (var i=0; i<8; i++){
+          this.height[i]=Math.round(this.towers[i].value/
+          (this.towers[i].maxValue-this.towers[i].minValue)/0.01);
+          console.log(this.height[i]);
+        }
       }
     )
   }
-  title = 'silo-towers';
+
+  ngOnInit() {
+     this.getData();
+
+  }
 }
-class Indicator{
+
+class Indicator {
   public id: string;
   public title: string;
   public value: number;
